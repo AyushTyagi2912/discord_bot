4ffb7f9e48ed5b30be930f1710197313c6485b7f
AyushTyagi2912 <90270512+AyushTyagi2912@users.noreply.github.com>
2021-09-21 20:54:26 +0530

    Add files via upload

---
+++ discord_bot/robot.py
@@ -0,0 +1,61 @@
+import discord
+from discord.ext import commands, tasks
+import random
+from itertools import cycle 
+
+client = commands.Bot(command_prefix='.')
+status = cycle(['sleeping','In coma'])
+#Events
+
+@client.event
+async def on_ready():
+    activity = discord.Game(name="DED", type=3)
+    await client.change_presence(activity=activity)
+    print("Bot is ready!")
+
+
+#Commands
+
+@client.command()
+async def intro(ctx):
+    await ctx.send(f"Hello Iam wankstabot my ping is...{round(client.latency * 1000)} ms")
+
+@client.command()
+async def Ques(ctx, *, question):
+    responses = ['No', 'Yes']
+    
+    await ctx.send(f"Question: {question}\nAnswer:{random.choice(responses)}")
+
+
+@client.command()
+async def kick(ctx,Member : discord.Member, *, reason=None):
+    await Member.kick(reason=reason)
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'The Ban Hammer has Spoken! {member.display_name} has been banned!')
+
+
+@client.event
+async def on_command_error(ctx,error):
+  if isinstance(error, commands.MissingRequiredArgument):
+    await ctx.send('please pass in an amount')  
+
+@client.command()
+async def clear(ctx, amount:int):
+    await ctx.channel.purge(limit = amount)
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.run('ODg1NDEzNjAzNDUxMjQwNDU5.YTmrrQ.vE9eHltIsClpEAmQShMHn-_ZnDM')
